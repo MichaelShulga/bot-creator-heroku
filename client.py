190bfda636221ef79f1b68e3cd3f291b7bde5402
MichaelShulga <anatemsf@yandex.ru>
2021-04-21 20:09:25 +0300

    before huge change

---
+++ client.py
@@ -0,0 +1,27 @@
+import json
+import os
+
+
+class Client:
+    def __init__(self, package):
+        self.path = os.path.join(package, "additional.json")
+
+    def settings(self):
+        with open(self.path) as additional:
+            return json.load(additional)
+
+    def update_settings(self, settings):
+        with open(self.path, 'w') as additional:
+            json.dump(settings, additional)
+
+    def set_running(self, flag):
+        settings = self.settings()
+        settings['running'] = flag
+        self.update_settings(settings)
+
+
+if __name__ == '__main__':
+    client = Client('client_settings')
+    a, b, c = client.settings().values()
+    client.set_running(True)
+    print(client.settings())
---
+++ client_settings/bot.py
@@ -0,0 +1,30 @@
+from vk_bot import VkGroup
+
+
+class EventsHandler(VkGroup):
+    def if_new_message(self, message, from_id, event):
+        if message == 'Привет':
+            answer = 'Ку'
+        else:
+            answer = 'Пошел в жопу'
+        self.send_message(to_id=from_id, message=answer)
+
+    def own_handler(self, event):
+        # https://vk.com/dev/groups_events
+        pass
+
+
+class OwnFunctions(VkGroup):
+    def send_date_to_me(self):
+        message = "Проверка бота 7:30"
+        self.send_message(to_id=315336001, message=message)
+
+
+class SchedulerSettings(OwnFunctions):
+    def activate_scheduler(self):
+        self.scheduler.add_job(self.send_date_to_me, 'interval', days=1, start_date='2021-04-21 07:30:00')
+        self.scheduler.start()
+
+
+class ResultVKClientGroup(EventsHandler, SchedulerSettings):
+    pass
